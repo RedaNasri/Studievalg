b62aaaaffb02748f49d583a6eb9277ecbd2b1d6b
Reda <redachikh@hotmail.no>
2026-05-05 05:21:44 +0200

    sitemap lagt til

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://studiematch.no',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
